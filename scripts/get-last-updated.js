4b56f11562767b3100167c83d586ece7cfe1f773
Lucas Nethercott <luke@nethercott.com>
2023-01-05 08:40:42 -0700

    fixed comments and some styling

--- scripts/get-last-updated.js
+++ scripts/get-last-updated.js
@@ -1,28 +1,29 @@
 function getLastUpdated() {
-    console.log('hello world')
-    // Fetch the latest commit of main branch from GitHub using the Git Data API
+    //Fetch the latest commit of main branch from GitHub using the Git Data API
     fetch('https://api.github.com/repos/lsneth/wdd230/branches/main')
         .then(response => response.json())
         .then(branch => {
-            // Extract the date and time of the most recent commit
-            let lastUpdated = branch.commit.commit.author.date;
-            const date = new Date(lastUpdated);
+            //Extract the date and time
+            let lastUpdated = branch.commit.commit.author.date
+            const date = new Date(lastUpdated)
 
+            //Format date and time
             const formattedDate = date.toLocaleDateString("en-US", {
             year: "numeric",
             month: "numeric",
             day: "numeric",
-            });
+            })
             const formattedTime = date.toLocaleTimeString("en-US", {
             hour: "numeric",
             minute: "numeric",
             second: "numeric",
-            });
+            })
 
-            const formattedDateTime = `${formattedDate} ${formattedTime}`;
+            const formattedDateTime = `${formattedDate} ${formattedTime}`
 
-            document.getElementById('last-updated').innerHTML = formattedDateTime;
-    });
+            //display formatted date and time in #last-updated element
+            document.getElementById('last-updated').textContent = formattedDateTime
+    })
 }
-// Call the getLastUpdated() function when the page loads
-window.onload = getLastUpdated;
+//Call the getLastUpdated() function when the page loads
+window.onload = getLastUpdated
