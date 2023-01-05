d661091d239ec666543160764b345f7f262a45d8
Lucas Nethercott <luke@nethercott.com>
2023-01-05 07:13:47 -0700

    another date/time test

--- scripts/getLastUpdated.js
+++ scripts/getLastUpdated.js
@@ -1,14 +1,14 @@
 function getLastUpdated() {
     // Fetch the commit history from GitHub using the Git Data API
-    fetch('https://api.github.com/repos/lsneth/wdd230/commits')
+    fetch('https://api.github.com/repos/lsneth/wdd230/branches/main')
         .then(response => response.json())
-        .then(commits => {
+        .then(branch => {
             // Extract the date and time of the most recent commit
-            let lastUpdated = commits[0].commit.committer.date;
+            console.log(branch)
+            let lastUpdated = branch.commit.commit.author.date;
             // Update the HTML element with the extracted date and time
             document.getElementById('last-updated').innerHTML = lastUpdated;
     });
-    console.log(response)
 }
 // Call the getLastUpdated() function when the page loads
 window.onload = getLastUpdated;
