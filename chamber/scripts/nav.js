042b7aa0af4c10536dc38c89663a383b0090322c
Lucas Nethercott <luke@nethercott.com>
2023-01-21 09:03:21 -0700

    chamber of commerce home page start

---
+++ chamber/scripts/nav.js
@@ -0,0 +1,6 @@
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a simple arrow function that listens for the <li> hamburger button click event.
+// When clicked, the <ul class="navigation">'s class list toggle'
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
