8cea8515f25b697f25bd19eb9acd06b4a009ddb3
Lucas <luke@nethercott.com>
2023-02-27 18:27:47 -0700

    publishing to test api call

---
+++ lesson9/scripts/prophets.js
@@ -0,0 +1,9 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json'
+
+async function getProphetData() {
+    const response = await fetch(url)
+    const data = await response.json()
+    console.table(data.prophets)
+  }
+  
+getProphetData()
