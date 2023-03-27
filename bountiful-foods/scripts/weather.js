509c8f78a894d8b3d0ffc5bd6ccc4bc205fd9820
Lucas Nethercott <luke@nethercott.com>
2023-03-27 17:10:05 -0600

    more weather updates

--- bountiful-foods/scripts/weather.js
+++ bountiful-foods/scripts/weather.js
@@ -1,13 +1,12 @@
 const API_KEY='11b458d7fbab78d318815a683f938f58'
 const weatherUrl=`https://api.openweathermap.org/data/2.5/weather?zip=92008&units=imperial&appid=${API_KEY}&cnt=3`
-const forecastUrl=`api.openweathermap.org/data/2.5/forecast?lat=33.14&lon=117.32&appid=${API_KEY}`
+const forecastUrl=`https://api.openweathermap.org/data/2.5/forecast?lat=33.1412&lon=117.3205&units=imperial&appid=${API_KEY}`
 
 async function getWeather() {
   try {
     const response = await fetch(weatherUrl)
     if (response.ok) {
       const data = await response.json()
-      console.log(data)
       return {temperature:data.main.temp, description:data.weather[0].description, humidity:data.main.humidity }
     } else {
         throw Error(await response.text())
@@ -22,8 +21,23 @@ async function getForecast() {
     const response = await fetch(forecastUrl)
     if (response.ok) {
       const data = await response.json()
-      console.log(data)
-      return {}
+      const forecastOne = data.list.slice(0,8)
+      const forecastTwo = data.list.slice(8,16)
+      const forecastThree = data.list.slice(16,24)
+    return {
+        dayOne:{
+          dayName:getDayOfWeek(forecastOne[0].dt_txt.slice(0,10)),
+          ...getHighAndLow(forecastOne)
+        },
+        dayTwo:{
+          dayName:getDayOfWeek(forecastTwo[0].dt_txt.slice(0,10)),
+          ...getHighAndLow(forecastTwo)
+        },
+        dayThree:{
+          dayName:getDayOfWeek(forecastThree[0].dt_txt.slice(0,10)),
+          ...getHighAndLow(forecastThree)
+        }
+      }
     } else {
         throw Error(await response.text())
     }
@@ -31,9 +45,23 @@ async function getForecast() {
       console.log(error)
   }
 }
-  
+
+function getDayOfWeek(date) {
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  return daysOfWeek[new Date(date).getDay()+1];
+}
+
+function getHighAndLow(forecastData) {
+  const highs = forecastData.map(data => data.main.temp_max)
+  const lows = forecastData.map(data => data.main.temp_min)
+  const high = Math.max(...highs)
+  const low = Math.min(...lows)
+  return {high, low}
+}
   
 function displayResults(weatherData) {
+  const currentTemp = document.getElementById('current-temp')
+  currentTemp.textContent = `${weatherData.temperature}°`
   temp.textContent = weatherData.main.temp.toFixed(0)
   const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`
   const desc = weatherData.weather[0].description
@@ -41,5 +69,5 @@ function displayResults(weatherData) {
   weatherIcon.setAttribute('alt', desc)
   currentCondition.textContent = weatherData.weather[0].description
 }
-  
-getForecast()
+
+getWeather().then(res => displayResults(res))
